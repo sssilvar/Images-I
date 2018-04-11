06c78c77900cc2b3187b928039795b82e985b452
Santiago Smith <sssilvar@unal.edu.co>
2018-04-11 14:09:43 -0500

    Curvelab test

--- pyCurvelab/curvelets_2d.py
+++ pyCurvelab/curvelets_2d.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
 
         # Go over all the angles in the scale
         for angle in angles:
-            if scale == 0:
+            if scale == 1:
                 ix = A.index(scale, angle)
                 # Delete data for testing
                 f[ix[0]:ix[1]] = 0
