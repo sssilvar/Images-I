f29c3677bffded72e2a6418fcb41d6cacb1fb0b3
Santiago Smith <sssilvar@unal.edu.co>
2018-04-11 10:00:09 -0500

    Curvelab test

--- pyCurvelab/curvelets_3d.py
+++ pyCurvelab/curvelets_3d.py
@@ -11,7 +11,7 @@ root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 if __name__ == '__main__':
     print('Loading image...')
     filename = os.path.join(root, 'data', 'brainmask.mgz')
-    img = nb.load(filename).get_data().astype(np.float)
+    img = nb.load(filename).get_data().astype(np.float)[50:150, 50:150, 150:150]
 
     # Setup curvelet params
     A = ct.fdct3(img.shape, nbs=2, nba=360, ac=True, norm=False, vec=True, cpx=False)
@@ -27,9 +27,9 @@ if __name__ == '__main__':
     y = A.inv(f)
 
     fig, ax = plt.subplots(1, 2, sharey='all')
-    ax[0].imshow(img[:, :, 128], cmap='gray')
+    ax[0].imshow(img[:, :, 50], cmap='gray')
     ax[0].set_title('Original')
 
-    ax[1].imshow(y[:, :, 128], cmap='gray')
+    ax[1].imshow(y[:, :, 50], cmap='gray')
     ax[1].set_title('Reconstructed')
     plt.show()
