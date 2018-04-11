c158ee570af21e033ebbfcc549ccf91fe7d7d86d
Santiago Smith <sssilvar@unal.edu.co>
2018-04-11 09:34:02 -0500

    Curvelab test

--- pyCurvelab/curvelets_2d.py
+++ pyCurvelab/curvelets_2d.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     img = imread(filename, as_grey=True)
 
     # Setup curvelet params
-    A = ct.fdct2(img.shape, nbs=2, nba=32, ac=True, norm=False, vec=True, cpx=False)
+    A = ct.fdct2(img.shape, nbs=2, nba=16, ac=True, norm=False, vec=True, cpx=False)
 
     # Apply curvelet to the image
     f = A.fwd(img)
