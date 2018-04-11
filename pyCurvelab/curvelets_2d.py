f5333058c8c4c2959f08ffc5d37a65658094bcb6
Santiago Smith <sssilvar@unal.edu.co>
2018-04-11 09:26:12 -0500

    Curvelab test

--- pyCurvelab/curvelets_2d.py
+++ pyCurvelab/curvelets_2d.py
@@ -6,7 +6,16 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     print('Loading image...')
-    img = imread('../opencv/img/brain.jpg')
+    img = imread('../opencv/img/brain.jpg', as_grey=True)
+
+    # Setup curvelet params
+    A = ct.fdtc(img.shape, nbs=2, nba=32, ac=True, norm=False, vec=True, cpx=False)
+
+    # Apply curvelet to the image
+    f = A.fwd(img)
+
+    print('Coefficients array:\n\t', f)
+    print('Shape: ', f.shape)
 
     plt.imshow(img)
     plt.show()
