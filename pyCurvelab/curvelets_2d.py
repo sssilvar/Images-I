71795bd3490ed4a026aaadebcb662a87554da7e3
Santiago Smith <sssilvar@unal.edu.co>
2018-04-11 14:16:03 -0500

    Curvelab test

--- pyCurvelab/curvelets_2d.py
+++ pyCurvelab/curvelets_2d.py
@@ -27,47 +27,48 @@ if __name__ == '__main__':
     number_of_scales = 7
     number_of_angles = 4
 
-    # Setup curvelet params
-    A = ct.fdct2(img.shape, nbs=number_of_scales, nba=number_of_angles, ac=True, norm=False, vec=True, cpx=False)
+    for s in range(0, number_of_scales):
+        # Setup curvelet params
+        A = ct.fdct2(img.shape, nbs=number_of_scales, nba=number_of_angles, ac=True, norm=False, vec=True, cpx=False)
 
-    # Apply curvelet to the image
-    f = A.fwd(img)
+        # Apply curvelet to the image
+        f = A.fwd(img)
 
-    print('Data coefficients type: ', type(f))
-    print('Coefficients array:\n\t', f)
-    print('Shape: ', f.shape)
-    print('Image shape vect: ', np.prod(img.shape))
+        print('Data coefficients type: ', type(f))
+        print('Coefficients array:\n\t', f)
+        print('Shape: ', f.shape)
+        print('Image shape vect: ', np.prod(img.shape))
 
-    # Print Information
-    for scale in range(0, number_of_scales):
-        if scale == 0:
-            angles = [0]
-        elif scale == 1:
-            angles = range(0, number_of_angles)
-        elif scale % 2 == 0:
-            angles = range(0, int(scale * number_of_angles))
-        elif scale % 2 != 0:
-            angles = range(0, int((scale - 1) * number_of_angles))
-        else:
-            angles = []
-            raise ValueError('There is no angles inside the scale')
+        # Print Information
+        for scale in range(0, number_of_scales):
+            if scale == 0:
+                angles = [0]
+            elif scale == 1:
+                angles = range(0, number_of_angles)
+            elif scale % 2 == 0:
+                angles = range(0, int(scale * number_of_angles))
+            elif scale % 2 != 0:
+                angles = range(0, int((scale - 1) * number_of_angles))
+            else:
+                angles = []
+                raise ValueError('There is no angles inside the scale')
 
-        # Go over all the angles in the scale
-        for angle in angles:
-            if scale == 2:
-                ix = A.index(scale, angle)
-                # Delete data for testing
-                f[ix[0]:ix[1]] = 0
-                print('Index: ', ix)
-            print_cl_info(f, scale, angle)
+            # Go over all the angles in the scale
+            for angle in angles:
+                if scale == 3:
+                    ix = A.index(scale)
+                    # Delete data for testing
+                    f[ix[0]:ix[1]] = 0
+                    print('Index: ', ix)
+                print_cl_info(f, scale, angle)
 
-    # Reconstruct the image
-    y = A.inv(f)
+        # Reconstruct the image
+        y = A.inv(f)
 
-    fig, ax = plt.subplots(1, 2, sharey='all')
-    ax[0].imshow(img, cmap='gray')
-    ax[0].set_title('Original')
+        fig, ax = plt.subplots(1, 2, sharey='all')
+        ax[0].imshow(img, cmap='gray')
+        ax[0].set_title('Original')
 
-    ax[1].imshow(y, cmap='gray')
-    ax[1].set_title('Reconstructed')
-    plt.show()
+        ax[1].imshow(y, cmap='gray')
+        ax[1].set_title('Reconstructed (Scale %d removed)' % s)
+        plt.show()
