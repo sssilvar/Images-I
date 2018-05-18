4e23273d43ca2bb6549c846dc2f24fe01cf045b4
Santiago S. Silva R <sssilvar@unal.edu.co>
2018-05-18 16:48:44 +0200

    ENIGMA docker finished

--- api/http_requests.py
+++ api/http_requests.py
@@ -1,23 +1,39 @@
 import numpy as np
 import requests
 
+
+def add_data(datafile):
+    data = np.load(datafile)
+
+    data_json = {
+        "currentAvgX": np.array(data['avx']).tolist(),
+        "currentStdX": np.array(data['stx']).tolist(),
+        "currentAvgY": np.array(data['avy']).tolist(),
+        "currentStdY": np.array(data['sty']).tolist(),
+        "updatedBy": '5afee47413755a1699c059dd',
+        "busy": False
+    }
+    print(data_json)
+
+    req = requests.post(server, json=data_json)
+    return req.status_code
+
+
 if __name__ == '__main__':
     server = 'http://localhost:3300/centers/stats'
 
-    data = np.random.normal(10, 2, [10, 10, 10])
-    dlist = data.tolist()
-
-    data_json = {
-        "currentAvg": dlist,
-        "currentStd": [10, 20, 30],
-        "busy": False,
-    }
-
-    requests.post(server, json=data_json)
+    datafile = '/user/ssilvari/home/Documents/temp/output/plsr/plsr_results.npz'
 
     r = requests.get(server)
-    a = r.json()
-    arr = a['msg']['currentAvg']
-    print(np.array(arr))
-    print('Shape: ', np.shape(arr))
-    print('Shape original: ', np.shape(dlist))
+    print(r.status_code)
+    r_json = r.json()
+
+    if r.status_code is 200:
+        if r_json['msg'] is not None:
+            arr = r_json['msg']['currentAvgY']
+            print(np.array(arr))
+            print('Shape: ', np.shape(arr))
+            add_data(datafile)
+        else:
+            print('Looks like there is nothing in the DB')
+            add_data(datafile)
