f6113c20a0f89ddd2fa96a12d3861217ca13523e
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-06-22 15:26:37 +0200

    ADMM - first try

--- admm/run_oarsub.sh
+++ admm/run_oarsub.sh
@@ -2,6 +2,11 @@
 
 #oarsub -I -p "gpu='YES' and gpucapability >= '5.0'" -l /gpunum=1,walltime=3:00:00
 #source ~/Softwares/virt_tf/bin/activate
-module load cuda/9.1
-module load cudnn/7.0-cuda-9.1
-module load tensorflow/1.6-python3-cuda9.1
+#module load cuda/9.1
+#module load cudnn/7.0-cuda-9.1
+#module load tensorflow/1.6-python3-cuda9.1
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+module load cuda/8.0
+CMD="python "${CURRENT_DIR}"/admm_pytorch.py"
