af49d93fd9283d431cb113c025515dc9a3a79984
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-08-10 18:05:09 +0200

    Trying to make a PLS

--- cross_decomposition_methods/pls.py
+++ cross_decomposition_methods/pls.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sklearn.cross_decomposition import PLSCanonical, PLSRegression, CCA
+import pandas as pd
+from sklearn import datasets
 
 import matplotlib.pyplot as plt
 
@@ -7,39 +8,43 @@ plt.style.use('ggplot')
 
 
 if __name__ == '__main__':
-    # #############################################################################
-    # PLS regression, with multivariate response, a.k.a. PLS2
+    # Load dataset
+    iris = datasets.load_iris()
+    df = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
+                     columns= iris['feature_names'] + ['target'])
+    print(df.head())
 
-    n = 1000
-    q = 3
-    p = 10
-    np.random.seed(42)
-
-    X = np.random.normal(size=n * p).reshape((n, p))
-    B = np.array([[1, 2] + [0] * (p - 2)] * q).T
-    # each Yj = 1*X1 + 2*X2 + noize
-    Y = np.dot(X, B) + np.random.normal(size=n * q).reshape((n, q)) + 5
-
-    pls2 = PLSRegression(n_components=3)
-    pls2.fit(X, Y)
-    print("True B (such that: Y = XB + Err)")
-    print(B)
+    # Plot Lenghts
+    fig, ax = plt.subplots()    
+    cax = ax.scatter(df['sepal length (cm)'], df['petal length (cm)'], c=df['target'], cmap='viridis')
     
-    # compare pls2.coef_ with B
-    print("Estimated B")
-    print(np.round(pls2.coef_, 1))
-    pls2.predict(X)
+    cbar = fig.colorbar(cax, ticks=[0, 1, 2], orientation='vertical')
+    cbar.ax.set_yticklabels(iris.target_names)  # horizontal colorbar
 
-    # PLS regression, with univariate response, a.k.a. PLS1
-    n = 1000
-    p = 10
+    # plt.show()
 
-    X = np.random.normal(size=n * p).reshape((n, p))
-    y = X[:, 0] + 2 * X[:, 1] + np.random.normal(size=n * 1) + 5
+    # Do PLS
+    X = np.array(df['sepal length (cm)'])
+    Y = np.array(df['petal length (cm)'])
 
-    pls1 = PLSRegression(n_components=3)
-    pls1.fit(X, y)
+    # A. Initialize u
+    u = np.array(Y[0])
+    err = 10
+
+    while err > 1e-4:
+        # B. X-weights
+        w = X.T.dot(u) / (u.T.dot(u))
+
+        # C. Calculate the X-scores
+        t = np.array(X.dot(w))
+
+        # D. Calculate Y-weigths
+        c = Y.T / t.T.dot(t)
+
+        # E. Update Y-scores
+        u = Y.dot(c) / c.T.dot(c)
+
+        
+        t_old = t.copy()
+
     
-    # note that the number of components exceeds 1 (the dimension of y)
-    print("Estimated betas")
-    print(np.round(pls1.coef_, 1))
