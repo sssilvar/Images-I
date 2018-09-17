448c94b8836ebf12322c47c9b5327a6bbb1c2134
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-09-17 16:38:38 +0200

    ADMM experiments

--- admm/admm_pytorch.py
+++ admm/admm_pytorch.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
 
             # legends.append('Rho = %.1E' % rho)
             # legends.append('%d centers' % m)
-            if exp_i % 10 == 0:
+            if exp_i in [i for i in range(9,10, n_experiments)]:
                 df_i = pd.DataFrame(err.numpy()[exp_i - 10:exp_i, :])
                 dfs.append(df_i)
                 legends.append('%d centers' % m)
