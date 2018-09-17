fe7bfa725c61698ad452b313367352b866fc5b75
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-09-17 17:27:10 +0200

    ADMM experiments

--- admm/admm_pytorch.py
+++ admm/admm_pytorch.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
             X = torch.randn(n, dx).type(dtype)
             W = torch.randn(dy, dx).t().type(dtype)
             Y = torch.mm(X, W).type(dtype)
-            Y = Y + torch.norm(Y) * torch.randn(n, dy).type(dtype)
+            Y = Y + 0.2 * torch.norm(Y) * torch.randn(n, dy).type(dtype)
 
             X_t_X, LU = torch.gesv(torch.eye(dx).type(dtype), torch.mm(X.t(), X))
             W_full_data = torch.mm(X_t_X, torch.mm(X.t(), Y))
