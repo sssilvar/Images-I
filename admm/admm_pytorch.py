872bb931e0652327d280f02f37612266a6a7149f
SILVA RINCON Santiago Smith <santiago-smith.silva-rincon@inria.fr>
2018-09-17 17:01:08 +0200

    ADMM experiments

--- admm/admm_pytorch.py
+++ admm/admm_pytorch.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
 
         plt.figure(figsize=(19.2 * 0.5, 10.8 * 0.5), dpi=150)
         for df in dfs:
-            # plt.fill_between([i for i in range(n_iter)], df.mean() - df.std(), df.mean() + df.std())
+            plt.fill_between([i for i in range(n_iter)], df.mean() - df.std(), df.mean() + df.std())
             plt.plot(df.mean())
         plt.xlabel('Number of iterations')
         plt.ylabel('Mean square error')
